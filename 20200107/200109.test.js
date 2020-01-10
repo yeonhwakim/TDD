de1e7344d30aaff93c0812b03c3606a6068393f5
karen <karen.kim@roumit.com>
2020-01-10 11:17:09 +0900

    프로그래머스 통과.
    중복되지 않은 경우도 true로 반환된다.
    경우의 수에 ""도 포함.

--- 20200107/200109.test.js
+++ 20200107/200109.test.js
@@ -5,8 +5,8 @@
 // 3. 2를 돌면서 1에 인덱스가 있는지 확인하며 개수를 구한다.  
 
 const getNumberOfCases = (skill) =>{
-  const numberOfCases = [];
-  [...skill].forEach((s, i) => numberOfCases.push((i ? numberOfCases[i-1] : '') + s));
+  const numberOfCases = [''];
+  [...skill].forEach((s, i) => numberOfCases.push((i+1 ? numberOfCases[i] : '') + s));
 
   return numberOfCases;
 };
